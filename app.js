7f3268cdd3f8d2bb0fe078c51882ba6bdb262314
Martin Chung <mwchung24@gmail.com>
2018-05-02 14:17:16 -0400

    connected pg to the node project

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const pg = require('pg');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+// Set up the express app
+const app = express();
+
+// Create connection to the PostgreSQL database
+const connect = "postgres://fortNiteTracker:T3sting!@localhost:5432/fortNiteTracker";
+
+app.get('/', function(req, res) {
+  pg.connect(connect, function(err, client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query('SELECT * FROM users', function(err, result) {
+      if(err) {
+        return console.error('error running query', err);
+      }
+      console.log({users: result.rows});
+      res.render('index', {users: result.rows});
+      done();
+    });
+  });
+});
+
+// Logs requests to the console
+app.use(logger('dev'));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Setup a default catch-all route that sends back a welcome message in JSON format.
+// app.get('*', (req, res) => res.status(200).send({
+//   message: 'Welcome to the beginning of nothingness.',
+// }));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +53,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
